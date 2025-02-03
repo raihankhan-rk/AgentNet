f5795a4d779580ee9dcf1c327fef0275191123c7
SayakSarkar02 <sayaksarkar73@gmail.com>
2025-02-04 01:21:48 +0530

    ui elements setup

---
+++ websites/chat/assets/AnimatedOrbs.tsx
@@ -0,0 +1,73 @@
+const AnimatedOrbs = () => {
+  return (
+      <>
+      <svg className=" absolute h-screen inset-0 z-[-3] overflow-hidden" width="1272" height="781" viewBox="0 0 1272 781" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_f_2005_93)">
+                <circle cx="762" cy="590" r="140" fill="#89BCFF"/>
+            </g>
+            <g filter="url(#filter1_f_2005_93)">
+                <circle cx="565" cy="707" r="207" fill="#FF86E1"/>
+            </g>
+            <defs>
+            <filter id="filter0_f_2005_93" x="322" y="150" width="880" height="880" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                <feGaussianBlur stdDeviation="150" result="effect1_foregroundBlur_2005_93"/>
+            </filter>
+            <filter id="filter1_f_2005_93" x="-142" y="0" width="1414" height="1414" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                <feGaussianBlur stdDeviation="250" result="effect1_foregroundBlur_2005_93"/>
+            </filter>
+            </defs>
+        </svg>
+
+        <svg className=" absolute h-screen inset-0 z-[-3] overflow-hidden" width="1272" height="781" viewBox="0 0 1272 781" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_f_2005_93)">
+                <circle cx="762" cy="590" r="140" fill="#89BCFF"/>
+            </g>
+            <g filter="url(#filter1_f_2005_93)">
+                <circle cx="565" cy="707" r="207" fill="#FF86E1"/>
+            </g>
+            <defs>
+            <filter id="filter0_f_2005_93" x="322" y="150" width="880" height="880" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                <feGaussianBlur stdDeviation="150" result="effect1_foregroundBlur_2005_93"/>
+            </filter>
+            <filter id="filter1_f_2005_93" x="-142" y="0" width="1414" height="1414" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                <feGaussianBlur stdDeviation="250" result="effect1_foregroundBlur_2005_93"/>
+            </filter>
+            </defs>
+        </svg>
+
+        <svg className=" absolute h-screen inset-0 -z-left overflow-hidden" width="1272" height="781" viewBox="0 0 1272 781" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_f_2005_93)">
+                <circle cx="762" cy="590" r="140" fill="#89BCFF"/>
+            </g>
+            <g filter="url(#filter1_f_2005_93)">
+                <circle cx="565" cy="707" r="207" fill="#FF86E1"/>
+            </g>
+            <defs>
+            <filter id="filter0_f_2005_93" x="322" y="150" width="880" height="880" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                <feGaussianBlur stdDeviation="150" result="effect1_foregroundBlur_2005_93"/>
+            </filter>
+            <filter id="filter1_f_2005_93" x="-142" y="0" width="1414" height="1414" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
+                <feGaussianBlur stdDeviation="250" result="effect1_foregroundBlur_2005_93"/>
+            </filter>
+            </defs>
+        </svg>
+        
+      </>
+        
+
+  );
+};
+
+export default AnimatedOrbs;
