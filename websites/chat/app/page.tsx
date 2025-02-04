73e411c2a5d4f18630fe622ec9206e899f9ebe16
Risav03 <needleweed03@gmail.com>
2025-02-05 02:27:54 +0530

    onramp done

--- websites/chat/app/page.tsx
+++ websites/chat/app/page.tsx
@@ -1,15 +1,17 @@
+'use client'
 import ChatArea from './components/ChatArea';
 import Sidebar from './components/Sidebar';
 import TextInput from './components/TextInput';
 import { ChatProvider } from './context/ChatContext';
 import AnimatedOrbs from '../assets/AnimatedOrbs';
+import { FundButton } from '@coinbase/onchainkit/fund';
 
 export default function Home() {
   return (
     <ChatProvider>
       <div className="flex h-screen w-screen overflow-hidden relative">
           <AnimatedOrbs />  
-       
+          {/* <FundButton className='absolute z-[10000] right-2 top-2'/> */}
         <Sidebar />
         <div className="flex flex-col flex-1 z-10">
           <ChatArea />
--- websites/chat/utils/providers.tsx
+++ websites/chat/utils/providers.tsx
@@ -8,6 +8,7 @@ export function Providers(props: { children: ReactNode }) {
   return (
     <OnchainKitProvider
       apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY}
+      projectId={process.env.NEXT_PUBLIC_CDP_PROJECT_ID}
       chain={baseSepolia} // add baseSepolia for testing
     >
       {props.children}
