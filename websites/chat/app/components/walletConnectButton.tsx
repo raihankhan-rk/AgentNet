3bfe6d5891ac06988ce9de07ca244cceac1bbb50
Risav03 <needleweed03@gmail.com>
2025-02-09 14:54:06 +0530

    FE changes to onchainkit stuff

--- websites/chat/app/components/walletConnectButton.tsx
+++ websites/chat/app/components/walletConnectButton.tsx
@@ -2,32 +2,37 @@
 import { ConnectWallet, Wallet, WalletDropdown, WalletDropdownDisconnect } from '@coinbase/onchainkit/wallet';
 import { base } from 'wagmi/chains';
 import { createSiweMessage } from 'viem/siwe'
-import { useSignMessage } from 'wagmi';
+import { useAccount, useSignMessage } from 'wagmi';
 import { Address, Avatar, EthBalance, Identity, Name } from '@coinbase/onchainkit/identity';
  
-const message = createSiweMessage({
-  address: '0xA0Cf798816D4b9b9866b5330EEa46a18382f251e',
-  chainId: base.id,
-  domain: 'example.com',
-  nonce: 'foobarbaz',
-  uri: 'https://example.com/path',
-  version: '1',
-})
+
  
 export function WalletComponents() {
+
+  const {address} = useAccount();
+
+  // const message = createSiweMessage({
+  //   address: address ? address as `0x${string}` : "0x1ce256752fBa067675F09291d12A1f069f34f5e8",
+  //   chainId: base.id,
+  //   domain: 'example.com',
+  //   nonce: 'foobarbaz',
+  //   uri: 'https://example.com/path',
+  //   version: '1',
+  // })
+
   const { signMessage } = useSignMessage();
  
   return (
-    <Wallet className='absolute z-[10000] right-2 top-2'>
-  <ConnectWallet onConnect={() => {signMessage({ message })}}>
-    <Avatar className="h-6 w-6" />
+    <Wallet className='text-black w-full hover:-translate-y-1 duration-200'>
+  <ConnectWallet className='hover:text-[#7B88F9] bg-gradient-to-br hover:text-[] from-[#abb3ff] to-[#fecdff] rounded hover:bg-[#7B88F9] w-full duration-200' >
+    {/* <Avatar className="h-6 w-6" /> */}
     <Name />
   </ConnectWallet>
-  <WalletDropdown>
-    <Identity className="px-4 pt-3 pb-2" hasCopyAddressOnClick>
-      <Avatar />
+  <WalletDropdown className='bg-[#7B88F9]'>
+    <Identity className="px-4 pt-3 pb-2 " hasCopyAddressOnClick>
+      <Avatar className="h-6 w-6" />
       <Name />
-      <Address />
+      <Address className=''/>
       <EthBalance />
     </Identity>
     <WalletDropdownDisconnect />
